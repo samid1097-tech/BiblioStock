eee27322d387a8a05e121b024e6d20b9cacc09a8
samid1097-tech <samid1097@gmail.com>
2026-09-20 22:28:31 -0500

    hicimos merge coon el avance de la rama de samid

--- funciones/registrar_prestamo.py
+++ funciones/registrar_prestamo.py
@@ -1,2 +1,13 @@
+import json
+
 def registrar_prestamo():
-    return "jeje"
+    try:
+        with open("inventario.json","r") as archivo:
+            libros=json.load(archivo)
+            print("libos con los que contamos disponibilidad:")
+            for libro in libros:
+                print(f"codigo{[codigo]}")
+    except FileNotFoundError:
+        print ("no contamos con libros disponible")
+
+registrar_prestamo()
